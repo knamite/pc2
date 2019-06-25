d94a28b57c3a76ac8daf033a04cb4beeadec99aa
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-25 08:29:25 -0500

    pregunta 3

--- Pregunta03.py
+++ Pregunta03.py
@@ -11,16 +11,17 @@ def aleatorio():
             n=n+[b]
     print(n)
 
-a=int(input("digite 6 numeros entre 1 al 10 :"))
-v=[]
-alea=[]
-if a>=1 and a<=10:
 
-    for i in range(1,7):
-        x=int(input("digite 6 numeros distintos entre 1 al 10 :"))
-        v=v+[x]
-    print("numeros salido ",v)  
-aleatorio()
+#a=int(input("digite 6 numeros entre 1 al 10 :"))
+v=[]
+#alea=[]
+#if a>=1 and a<=10:
+
+for i in range(1,7):
+    x=int(input("digite 6 numeros distintos entre 1 al 10 :"))
+    v=v+[x]
+print("numeros salido ",v)  
+
 
     
 
